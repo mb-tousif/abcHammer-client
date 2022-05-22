74e87fabc099d11ec39670d4fc18ad8e979a05fa
mb-tousif <meta.block.tousif@gmail.com>
2022-05-22 21:53:01 +0600

    Authentication Template Done

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Background from '../../Assets/Images/topb.jpg'
-import Button from '../SharedPages/Button/Button';
+
 const Banner = () => {
     return (
         <div className="hero min-h-screen" style={{ backgroundImage: `url(${Background})`, backgroundSize: 'cover'}}>
@@ -9,7 +9,7 @@ const Banner = () => {
             <div className="max-w-md">
                 <h1 className="mb-5 md:text-5xl text-xl font-bold">Welcome to ABC Hammer <br /> Manufacturing Ltd</h1>
                 <p className="mb-5">ABC Hammer Manufacturing Ltd. is the leading manufacturer of the highest quality, non-marring, non-sparking, non-magnetic and non-corrosive striking tools. We have been a family owned company for over 50 years.</p>
-                <Button/>
+                <button className="btn btn-primary">Get Purchased</button>
             </div>
           </div>
         </div>
--- src/components/userAuthentication/LogIn.js
+++ src/components/userAuthentication/LogIn.js
@@ -1,26 +1,33 @@
 import React from 'react';
+import { FcGoogle } from "react-icons/fc";
 
 const LogIn = () => {
     return (
-     <div class="hero min-h-screen bg-base-200">
-        <div class="hero-content flex-col lg:flex-row-reverse">
-         <div class="text-center lg:text-left">
-          <h1 class="text-5xl font-bold">Login now!</h1>
-          <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        </div>
+     <div class="hero bg-base-100">
+        <div class="hero-content w-full">
         <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
         <div class="card-body">
+          <h1 class="md:text-4xl text-2xl text-secondary font-bold">Log In</h1>
          <div class="form-control">
           <label class="label"><span class="label-text">Email</span></label>
-          <input type="text" placeholder="email" class="input input-bordered" />
+          <input type="text" placeholder="email" class="input input-bordered"/>
          </div>
         <div class="form-control">
           <label class="label"><span class="label-text">Password</span></label>
-          <input type="text" placeholder="password" class="input input-bordered" />
-          <label class="label"><a href="#" class="label-text-alt link link-hover">Forgot password?</a></label>
+          <input type="text" placeholder="password" class="input input-bordered"/>
         </div>
         <div class="form-control mt-6">
-          <button class="btn btn-primary">Login</button>
+          <button class="btn btn-primary">Log In</button>
+        </div>
+        <div class="flex items-center justify-center py-4 text-center bg-gray-50 dark:bg-gray-700">
+            <span class="text-sm text-gray-600 dark:text-gray-200">Don't have an account? </span>
+            <span href="#" class="mx-2 text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline">Register</span>
+        </div>
+        <div class="flex flex-col w-full border-opacity-50">
+          <div class="divider">OR</div>
+        </div>
+        <div className="">
+          <button class="btn btn-primary mb-4 w-full max-w-xs"><FcGoogle className='md:w-12 md:h-8 w-8 h-8'/>Continue with Google</button>
         </div>
       </div>
     </div>
